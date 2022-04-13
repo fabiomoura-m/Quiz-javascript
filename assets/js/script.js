159a4c7be88f6305c86d584f617c375513b6eff3
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-13 20:34:21 -0300

    feat: inclui função que mostra a pergunta na tela

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Initial Data
+let currentQuestion = 0;
+
+showQuestion();
+
+// Functions
+function showQuestion() {
+  if (questions[currentQuestion]) {
+    let q = questions[currentQuestion];
+
+    document.querySelector('.scoreArea').style.display = 'none';
+    document.querySelector('.questionArea').style.display = 'block';
+
+    document.querySelector('.question').innerHTML = q.question;
+  } else {
+    // acabaram as questoes
+  }
+}
