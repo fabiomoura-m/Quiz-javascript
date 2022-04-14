6df29653cee4cd430414155de7065e27c0f88c43
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-14 14:31:16 -0300

    feat: inclui barra de progressão dinâmica

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,20 @@ showQuestion();
 // Functions
 function showQuestion() {
   if (questions[currentQuestion]) {
-    let q = questions[currentQuestion];
+    let query = questions[currentQuestion];
+
+    let percentage = Math.floor((currentQuestion / questions.length) * 100);
+    document.querySelector('.progress--bar').style.width = `${percentage}%`;
 
     document.querySelector('.scoreArea').style.display = 'none';
     document.querySelector('.questionArea').style.display = 'block';
 
     document.querySelector('.question').innerHTML = q.question;
-
     let optionsHtml = '';
-    for (let i in q.options) {
+    for (let i in query.options) {
       optionsHtml += `<div data-op="${i}" class="option"><span>${
         parseInt(i) + 1
-      }</span>${q.options[i]}</div>`;
+      }</span>${query.options[i]}</div>`;
     }
     document.querySelector('.options').innerHTML = optionsHtml;
 
