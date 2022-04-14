c0dd2df5a0f37f0657fbb0b7e9c69f2287903aab
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-13 22:59:36 -0300

    feat: exibição das alternativas na tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,14 @@ function showQuestion() {
     document.querySelector('.questionArea').style.display = 'block';
 
     document.querySelector('.question').innerHTML = q.question;
+
+    let optionsHtml = '';
+    for (let i in q.options) {
+      optionsHtml += `<div class="option"><span>${parseInt(i) + 1}</span>${
+        q.options[i]
+      }</div>`;
+    }
+    document.querySelector('.options').innerHTML = optionsHtml;
   } else {
     // acabaram as questoes
   }
